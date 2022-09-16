e0a97bd601a8348d878fd11573f48924ea6dfb36
Jasmeet-st1 <98531221+Jasmeet-st1@users.noreply.github.com>
2022-09-16 14:54:55 +0530

    Add files via upload

--- project_2/scripts/home.js
+++ project_2/scripts/home.js
@@ -9,7 +9,7 @@ searchBtn.addEventListener('click',function(){
         searchInput.style.transform="translateY(8px)";
     }
     else if(x && searchInput.value!=""){
-        alert("Hello");
+        location.href="./single_type_product_list.html";
     }
     else if(x){
         searchInput.style.transition='0.3s ease-in-out';
@@ -17,19 +17,11 @@ searchBtn.addEventListener('click',function(){
     }
 });
 
-let header=document.querySelector("#header");
-let upperHead=document.querySelector("#upper_head");
-let headPos = upperHead.getBoundingClientRect().top;
+let topBtn=document.querySelector("#btt");
+topBtn.addEventListener('click',function(){
+    // let scrollPos = window.scrollY;
+    document.documentElement.scrollTop = 0;
 
-window.addEventListener("scroll", e => {
-    let scrollPos = window.scrollY;
-    if (scrollPos > headPos) {
-        upperHead.classList.add('sticky');
-        header.classList.add('headerOffsetMargin');
-    } else {
-        upperHead.classList.remove('sticky');
-        header.classList.remove('headerOffsetMargin');
-    }
 });
 
 let box=document.querySelectorAll(".outerbox");
@@ -40,3 +32,50 @@ window.addEventListener('load',function(){
         box[i].style.transform="translateY(0px)";
     }
 });
+
+//timer
+let offerDivs = document.querySelectorAll(".time-div");
+
+
+
+for(let i=0; i<offerDivs.length; i++){
+    var x = setInterval(xyz, 1000,i);
+}
+
+function xyz(i) {
+    
+    var now = new Date().getTime();
+    var end = new Date();
+    end.setUTCHours(18,29,59,999);
+    
+    var distance = end - now;
+    // console.log(end);
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    let Times=days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+    if(days==0){
+        Times = hours + "h " + minutes + "m " + seconds + "s ";
+    }
+    if(days==0 && hours==0){
+        Times = minutes + "m " + seconds + "s ";
+    }
+    if(days==0 && hours==0 && minutes==0){
+        Times = seconds + "s ";
+    }
+    offerDivs[i].innerHTML = Times;
+    if (distance < 0) {
+        clearInterval(x);
+        offerDivs[i].innerHTML = "EXPIRED";
+    }
+}
+
+
+let main_items=document.querySelectorAll("#main-items .col-lg-5");
+for(let i=0;i<main_items.length;i++){
+    main_items[i].style.backgroundImage="url('./photos/home_items/pic"+(i+1)+".jpg')"
+}
